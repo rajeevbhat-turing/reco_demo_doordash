aeb942f4368eb16729c274889525c458016a44da
samhirtb <samhir.tarif@gmail.com>
2025-11-24 18:13:06 +0500

    Add get_order method with aggregation based sorting. Remove get-cheapest-restaurant-by-frequent-cuisine

--- lib/verifier/expected-state-functions/index.ts
+++ lib/verifier/expected-state-functions/index.ts
@@ -2,10 +2,10 @@ export { get_default_payment_method } from './get-default-payment-method';
 export { get_deal } from './get-deal';
 export type { Deal } from './get-deal';
 export { get_user_address } from './get-user-address';
-export { get_cheapest_restaurant_by_frequent_cuisine } from './get-cheapest-restaurant-by-frequent-cuisine';
-export type { Restaurant, CheapestRestaurantResult } from './get-cheapest-restaurant-by-frequent-cuisine';
 export { get_restaurants } from './get-restaurants';
 export type { RestaurantResult, GetRestaurantsArgs, GetRestaurantsResult, SortSpec as RestaurantSortSpec } from './get-restaurants';
 export { get_items } from './get-items';
 export type { MenuItemResult, GetItemsArgs, GetItemsResult, SortSpec } from './get-items';
+export { get_orders } from './get-orders';
+export type { OrderResult, GetOrdersArgs, GetOrdersResult, SortSpec as OrderSortSpec } from './get-orders';
 
